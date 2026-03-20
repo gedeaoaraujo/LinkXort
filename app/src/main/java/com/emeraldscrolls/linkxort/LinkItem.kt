5ebdcc1c57576db158d8d08960c6ceb7028c2ef3
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-03-20 12:11:42 -0300

    Creating link item.

---
+++ app/src/main/java/com/emeraldscrolls/linkxort/LinkItem.kt
@@ -0,0 +1,58 @@
+package com.emeraldscrolls.linkxort
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LinkItem(item: String) {
+  val original = item
+  val alias = "123456789"
+  val urlShorted = "https://test.com/123456789"
+
+  Card(
+    modifier = Modifier.fillMaxWidth(),
+    shape = RoundedCornerShape(12.dp),
+    colors = CardDefaults.cardColors(
+      containerColor = MaterialTheme.colorScheme.surfaceVariant
+    )
+  ) {
+    Column(Modifier.padding(16.dp).fillMaxWidth()) {
+      Text(
+        fontWeight = FontWeight.Bold,
+        text = "Alias: ${alias}",
+        style = MaterialTheme.typography.titleSmall,
+        color = MaterialTheme.colorScheme.primary,
+      )
+      Spacer(modifier = Modifier.height(4.dp))
+      Text(
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        text = "Original: ${original}",
+        style = MaterialTheme.typography.bodyMedium,
+        color = MaterialTheme.colorScheme.outline,
+      )
+      Spacer(modifier = Modifier.height(4.dp))
+      Text(
+        maxLines = 2,
+        text = urlShorted,
+        fontWeight = FontWeight.Medium,
+        overflow = TextOverflow.Ellipsis,
+        style = MaterialTheme.typography.bodySmall,
+        color = MaterialTheme.colorScheme.onSurfaceVariant,
+      )
+    }
+  }
+}
